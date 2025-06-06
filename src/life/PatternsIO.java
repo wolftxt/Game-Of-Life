499058b12ef1c847c5157696ca52be73ad98c421
wolftxt <wolfdavid49@gmail.com>
2025-06-06 15:22:43 +0200

    Refactored code

--- src/life/PatternsIO.java
+++ src/life/PatternsIO.java
@@ -1,15 +1,24 @@
 package life;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class PatternsIO {
 
+    private static final String PATTERNDIRECTORY = "patterns";
+
     public static boolean save(boolean[][] array, String filename) {
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
+        File patterns = new File(getPatternsDirectory(), filename);
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(patterns))) {
             out.writeObject(array);
         } catch (IOException e) {
             return true;
@@ -18,8 +27,29 @@ public class PatternsIO {
     }
 
     public static boolean[][] load(String filename) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
-            return (boolean[][]) in.readObject();
+        File patterns = new File(getPatternsDirectory(), filename);
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(patterns))) {
+            return (boolean[][]) ois.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            downloadPatterns();
         }
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(patterns))) {
+            return (boolean[][]) ois.readObject();
+        }
+    }
+
+    private static void downloadPatterns() throws MalformedURLException, IOException {
+        URL url = new URL("https://github.com/wolftxt/Game-Of-Life/raw/refs/heads/master/src/life/patterns/help");
+        InputStream in = url.openStream();
+        File patterns = new File(getPatternsDirectory(), "help");
+        if (!patterns.exists()) {
+            patterns.mkdirs();
+        }
+        Files.copy(in, patterns.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public static File getPatternsDirectory() {
+        File codeDirectory = new File(LifeWindow.class.getProtectionDomain().getCodeSource().getLocation().getFile());
+        return new File(codeDirectory.getParent(), PATTERNDIRECTORY);
     }
 }
